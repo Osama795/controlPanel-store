41c43e4956b097d166937b3765b91c1cf55566ba
Your Name <you@example.com>
2023-12-21 14:19:38 +0200

    finish components

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     entry: {
       'app': './src/index.js',
       'assets/js/banner': './src/assets/js/banner.js',
+      'assets/js/tabs': './src/assets/js/tabs.js',
+      'assets/js/upload': './src/assets/js/upload.js',
+      'assets/js/chart': './src/assets/js/chart.js',
     },
     output: {
         publicPath: '/',
@@ -48,6 +51,21 @@ module.exports = {
           },
 
           {
+            test: /\.(png|svg|jpe?g|gif)$/,
+            exclude: /fonts/,
+            use: [
+              {
+                loader: "file-loader", 
+                options: {
+                  esModule: true,
+                  name: '[name].[ext]',
+                  outputPath: "assets/images",
+                }
+              }
+            ]
+          },
+
+          {
             test: /\.(svg|eot|woff|woff2|ttf)$/,
             exclude: /images/,
             use: [
@@ -79,9 +97,6 @@ module.exports = {
     // في حال واجهت مشكلة اختفاء ملف index.html الأساسي من ملفات البناء عدل خصائص إضافة مسح ملفات البناء clean-webpack-plugin كالتالي:
     // new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
         new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "assets/css/style.css"
-        }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html',
@@ -107,5 +122,55 @@ module.exports = {
         template: "./src/components/banner.html",
         chunks: ['app', 'assets/js/banner']
       }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/list.html",
+        template: "./src/components/list.html",
+        chunks: ['app']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/tabs.html",
+        template: "./src/components/tabs.html",
+        chunks: ['app', 'assets/js/tabs']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/upload.html",
+        template: "./src/components/upload.html",
+        chunks: ['app', 'assets/js/upload']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/help.html",
+        template: "./src/components/help.html",
+        chunks: ['app']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/summary.html",
+        template: "./src/components/summary.html",
+        chunks: ['app']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/actions.html",
+        template: "./src/components/actions.html",
+        chunks: ['app']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/sidebar.html",
+        template: "./src/components/sidebar.html",
+        chunks: ['app']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/table.html",
+        template: "./src/components/table.html",
+        chunks: ['app']
+      }),
+      new HtmlWebpackPlugin({ 
+        filename: "components/chart.html",
+        template: "./src/components/chart.html",
+        chunks: ['app', 'assets/js/chart']
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: "assets/css/style.css"
+      }),
+      new OptimizeCSSAssetsPlugin({}),
     ]
 }
